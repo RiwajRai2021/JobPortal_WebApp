bc29dcc7703a9c899df197ee1a849667641a9c2c
RiwajRai2021 <riwajrai83@gmail.com>
2025-06-01 22:08:15 -0400

    added initial jobseeker and recruiter feature along with security loging

--- jobportal/src/main/java/com/project/jobportal/services/UsersService.java
+++ jobportal/src/main/java/com/project/jobportal/services/UsersService.java
@@ -1,6 +1,10 @@
 package com.project.jobportal.services;
 
+import com.project.jobportal.entity.JobSeekerProfile;
+import com.project.jobportal.entity.RecruiterProfile;
 import com.project.jobportal.entity.Users;
+import com.project.jobportal.repository.JobSeekerProfileRepository;
+import com.project.jobportal.repository.RecruiterProfileRepository;
 import com.project.jobportal.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,16 +16,29 @@ import java.util.Optional;
 public class UsersService {
 
     private final UsersRepository usersRepository;
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
+    private final RecruiterProfileRepository recruiterProfileRepository;
 
 
     @Autowired
-    public UsersService(UsersRepository usersRepository) {
+    public UsersService(UsersRepository usersRepository,
+                        JobSeekerProfileRepository jobSeekerProfileRepository,
+                        RecruiterProfileRepository recruiterProfileRepository) {
         this.usersRepository = usersRepository;
+        this.jobSeekerProfileRepository=jobSeekerProfileRepository;
+        this.recruiterProfileRepository=recruiterProfileRepository;
     }
 
     public Users addNew(Users users){
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
+        Users savedUsers = usersRepository.save(users);
+        int userTypeId = users.getUserTypeId().getUserTypeId();
+        if(userTypeId == 1){
+            recruiterProfileRepository.save(new RecruiterProfile(savedUsers));
+        }else{
+            jobSeekerProfileRepository.save(new JobSeekerProfile(savedUsers));
+        }
         return usersRepository.save(users);
     }
 
